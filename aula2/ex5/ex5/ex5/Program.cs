5882d5f39a26ab4d131146889c6b16a29c4ee211
marcosimao12 <marcosimaocarvalho@gmail.com>
2022-02-23 16:55:28 +0000

    ex6

--- aula2/ex5/ex5/ex5/Program.cs
+++ aula2/ex5/ex5/ex5/Program.cs
@@ -1,7 +1,1 @@
 ﻿
-
-//exe 5
-
-Console.WriteLine("Ana Ruivo \t Porto");
-Console.WriteLine("Joaquina Pinto \t Aveiro");
-Console.WriteLine("Miguel Costa \t Braga");
---
+++ aula2/ex6/ex6/ex6/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine("4 + 3 x (15 - 2) = {0}", 4 + 3 * (15 - 2));
+Console.WriteLine("40 / 9 = {0}", Math.Round((float)40 / 9, 2));
+Console.WriteLine("Resto da divisão de 40 por 3 = {0}", 40 % 3);
