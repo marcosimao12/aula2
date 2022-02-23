edfdfc8ffeeb20582101b48bc1b66549fe450fc9
marcosimao12 <marcosimaocarvalho@gmail.com>
2022-02-23 15:44:06 +0000

    ex2

---
+++ aula2/ex1/EX2/EX2/Program.cs
@@ -0,0 +1,10 @@
+﻿
+public class Example
+{
+    public static void Main()
+    {
+        string nome = "alunos";
+        string nome2 =  "Bem vindos" ;
+        Console.WriteLine(" Caros  "+nome+" sejam "+nome2+" ao C#");
+    }
+}
