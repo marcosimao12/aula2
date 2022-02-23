289498896def3961654343a535950d3675efe3db
marcosimao12 <marcosimaocarvalho@gmail.com>
2022-02-23 15:53:29 +0000

    ex3

---
+++ aula2/ex3/ex3/ex3/Program.cs
@@ -0,0 +1,4 @@
+﻿
+    Console.WriteLine("Ana Ruivo".PadRight(20) + "Porto".PadRight(10));
+    Console.WriteLine("Joaquina Pinto".PadRight(20) + "Aveiro".PadRight(10));
+    Console.WriteLine("Miguel Costa".PadRight(20) + "Braga".PadRight(10));
